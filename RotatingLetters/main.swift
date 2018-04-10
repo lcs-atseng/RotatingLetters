784432d56782788754d4d0394cf1f29d14a6816d
Alice Tseng <atseng@lcs-mba3235.lcs.on.ca>
2018-04-10 14:55:16 -0400

    So far prompt

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -7,11 +7,30 @@
 //
 
 import Foundation
-
-// Get the user input
-var rawInput = readLine()
-
-// Print out the input provided
-print("You said:")
-print(rawInput)
+while 1 == 1 {
+    //INPUT
+    
+    // loop forever until we get good input
+    
+    // Prompt the user
+    print("Input word is?")
+    
+    // Wait for input
+    // Test #1: make sure it is not nil
+    guard let givenInput = readLine() else {
+        
+        // If we got here, input is nil
+        //prompt again
+   continue  // continue with the next iteration of while Input
+    }
+    // Test #2: Is the input the correct length
+    if givenInput.count < 1 || givenInput.count > 30 {
+        
+        //if we got here, input is too short
+        continue // prompt again
+    }
+    
+    //If we got to this point, we know the input is good
+    break // quit the while loop and begin PROCESS section
+}
 
